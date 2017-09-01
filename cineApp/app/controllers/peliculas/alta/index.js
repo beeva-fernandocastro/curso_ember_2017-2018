73490b62a95e6ad610aff1daff51568c669eb0c8
Juan Jose Antequera Flores <juanjose.antequera@bbva.com>
2017-09-01 20:53:41 +0200

    Formulario de alta

--- cineApp/app/controllers/peliculas/alta/index.js
+++ cineApp/app/controllers/peliculas/alta/index.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    guardar() {
+
+      this.transitionToRoute('peliculas.alta.confirmacion');
+    }
+  }
 });
